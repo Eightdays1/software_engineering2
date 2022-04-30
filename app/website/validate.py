bd2f7fb3f3bd4d1059fcf830c2aa44f39fa59bc5
Eightdays <kevin.noisternig@web.de>
2022-05-01 00:25:20 +0200

    code smell: large class und Duplicated Code + refactoring

--- app/website/validate.py
+++ app/website/validate.py
@@ -2,31 +2,38 @@ import re
 
 
 class Validate:
-    def __init__(self, to_validate):
+    def __init__(self, to_validate, regex):
         self.to_validate = to_validate
+        self.regex = regex
 
-    def password(self):
-        regex = "[A-Za-z0-9@#$%^&+=_]{8,150}"
-        if re.fullmatch(regex, self.to_validate):
+    def check(self):
+        if re.fullmatch(self.regex, self.to_validate):
             return True
         return False
 
-    def name(self):
-        regex = "[A-za-z ,-ÄÖÜäöüß]{3,150}"
-        if re.fullmatch(regex, self.to_validate):
-            return True
-        return False
 
-    def email(self):
-        regex = "([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+.([A-Z|a-z]{2,})+"
-        if re.fullmatch(regex, self.to_validate):
-            return True
-        return False
+class ValidatePassword(Validate):
 
-    def group_name(self):
-        regex = "[A-za-z ,-ÄÖÜäöüß0-9]{3,100}"
-        if re.fullmatch(regex, self.to_validate):
-            return True
-        return False
+    def __init__(self, to_validate):
+        super().__init__(to_validate, "[A-Za-z0-9@#$%^&+=_]{8,150}")
+
+
+class ValidateName(Validate):
+
+    def __init__(self, to_validate):
+        super().__init__(to_validate, "[A-za-z ,-ÄÖÜäöüß]{3,150}")
+
+
+class ValidateEmail(Validate):
+
+    def __init__(self, to_validate):
+        super().__init__(to_validate, "([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+.([A-Z|a-z]{2,})+")
+
+
+class ValidateGroupname(Validate):
+
+    def __init__(self, to_validate):
+        super().__init__(to_validate, "[A-za-z ,-ÄÖÜäöüß0-9]{3,100}")
+
 
 
